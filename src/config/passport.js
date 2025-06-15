468203fbeea7c3b762172505958296482c33ebd8
AniketKhonde <aniketkhonde51@gmail.com>
2025-06-15 23:14:03 +0530

    type fix

--- src/config/passport.js
+++ src/config/passport.js
@@ -19,7 +19,10 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback',
+    // callbackURL: '/api/auth/google/callback',
+    callbackURL: process.env.NODE_ENV === 'production' 
+        ? `${process.env.BACKEND_URL}/api/auth/google/callback`
+        : '/api/auth/google/callback',
     scope: ['profile', 'email']
 },
 async function(accessToken, refreshToken, profile, done) {
